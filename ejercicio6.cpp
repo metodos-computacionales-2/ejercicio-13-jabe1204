343563318aad4cda2c8e60caead327f220ddf120
jabe1204 <56890887+jabe1204@users.noreply.github.com>
2019-11-13 12:41:46 -0500

    Ejercicio6

---
+++ ejercicio6.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+int main(int argc, char **argv)
+{
+    int N=atoi(argv[1]); //casting to int
+    double array[N];
+    for(int i =0;i<N;i++)
+    {
+        array[i]=i;
+    }
+    return 0;
+}
